01ed090843dbb50f0d7f75142be4b2b4ea4175e8
yannFerPuig <159564668+yannFerPuig@users.noreply.github.com>
2024-04-21 18:47:44 +0200

    Ajout du timer

---
+++ Kick Out!/Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+
+    public float remainingTime;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0) 
+        {
+            remainingTime = 0;
+        }
+
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int secondes = Mathf.FloorToInt(remainingTime % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, secondes);        
+    }
+}
