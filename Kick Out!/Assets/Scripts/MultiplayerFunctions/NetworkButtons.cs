1dcaa4f6c189abe6a584edf70c017b19fad16e74
Ethanbobol <ethan.carrouee@epita.fr>
2024-06-13 18:00:21 +0200

    remodif

--- Kick Out!/Assets/Scripts/MultiplayerFunctions/NetworkButtons.cs
+++ Kick Out!/Assets/Scripts/MultiplayerFunctions/NetworkButtons.cs
@@ -7,14 +7,8 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NetworkButtons : NetworkBehaviour
+public class NetworkButtons : MonoBehaviour
 {
-    public string sceneToLoad = "YourSceneName";
-
-    private void ChangeScene()
-    {
-        NetworkManager.SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
-    }
 
     void OnGUI()
     {
@@ -24,7 +18,6 @@ public class NetworkButtons : NetworkBehaviour
             if (GUILayout.Button("Host")) 
             {
                 NetworkManager.Singleton.StartHost();
-                ChangeScene();
             }
             if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
         }
