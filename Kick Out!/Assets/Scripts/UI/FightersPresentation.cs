76ea3f5b9f7c978f24380c03a1ea62bf6f625f82
Subelf2 <163747154+Subelf2@users.noreply.github.com>
2024-06-15 22:38:31 +0200

    fixed all scenes

--- Kick Out!/Assets/Scripts/UI/FightersPresentation.cs
+++ Kick Out!/Assets/Scripts/UI/FightersPresentation.cs
@@ -3,8 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class FightersPresentation : MonoBehaviour
 {
+    public MainMenu menu;
     public void ChangeScene(string scene)
     {
+
         SceneManager.LoadSceneAsync(scene);
+    }
+
+    public void Tutorial()
+    {
+        menu = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<MainMenu>();
+        menu.gameMode = "tutorial";
+        SceneManager.LoadScene("SoloCharacterSelection");
     }
 }
